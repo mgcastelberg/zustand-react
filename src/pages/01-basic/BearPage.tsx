6b579b1c481a9be6af01f2a1b25951ba7ab10858
Manuel Gomez <jmgc@virket.com>
2024-04-29 20:36:30 -0600

    Instalación zustand, creacion y cosumo de nuestro primer store

--- src/pages/01-basic/BearPage.tsx
+++ src/pages/01-basic/BearPage.tsx
@@ -1,4 +1,5 @@
 import { WhiteCard } from '../../components';
+import { useBearStore } from '../../stores';
 
 export const BearPage = () => {
   return (
@@ -6,48 +7,58 @@ export const BearPage = () => {
       <h1>Contador de Osos</h1>
       <p>Manejo de estado simple de Zustand</p>
       <hr />
-
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
-
-        <WhiteCard centered>
-          <h2>Osos Negros</h2>
-
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-          </div>
-
-        </WhiteCard>
-
-        <WhiteCard centered>
-          <h2>Osos Polares</h2>
-
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-
-          </div>
-
-        </WhiteCard>
-
-        <WhiteCard centered>
-          <h2>Osos Pandas</h2>
-
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-          </div>
-
-        </WhiteCard>
-
-
-
-
+      <div className="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3">
+        <BlackBears/>
+        <PolarBears/>
+        <PandaBears/>
       </div>
-
     </>
   );
 };
+
+export const BlackBears = () => {
+  const blackBears = useBearStore( state => state.blackBears );
+  const increaseBlackBears = useBearStore( state => state.increaseBlackBears );
+  // No es conveniente usar este tipo de desestructuracion ya que rerenderiza con cualquier cambio de otro componente
+  // const { blackBears, increaseBlackBears} = useBearStore( state => state);
+  return (
+    <WhiteCard centered>
+    <h2>Osos Negros</h2>
+    <div className="flex flex-col md:flex-row">
+      <button onClick={ () => increaseBlackBears(+1) }> +1</button>
+      <span className="mx-2 text-3xl lg:mx-10"> { blackBears } </span>
+      <button onClick={ () => increaseBlackBears(-1) }>-1</button>
+    </div>
+  </WhiteCard>
+  )
+}
+
+export const PolarBears = () => {
+  const polarBears = useBearStore( state => state.polarBears );
+  const increasePolarBears = useBearStore( state => state.increasePolarBears );
+  return (
+    <WhiteCard centered>
+    <h2>Osos Negros</h2>
+    <div className="flex flex-col md:flex-row">
+      <button onClick={ () => increasePolarBears(+1) }> +1</button>
+      <span className="mx-2 text-3xl lg:mx-10"> { polarBears } </span>
+      <button onClick={ () => increasePolarBears(-1) }>-1</button>
+    </div>
+  </WhiteCard>
+  )
+}
+
+export const PandaBears = () => {
+  const pandaBears = useBearStore( state => state.pandaBears );
+  const increasePandaBears = useBearStore( state => state.increasePandaBears );
+  return (
+    <WhiteCard centered>
+    <h2>Osos Negros</h2>
+    <div className="flex flex-col md:flex-row">
+      <button onClick={ () => increasePandaBears(+1) }> +1</button>
+      <span className="mx-2 text-3xl lg:mx-10"> { pandaBears } </span>
+      <button onClick={ () => increasePandaBears(-1) }>-1</button>
+    </div>
+  </WhiteCard>
+  )
+}
