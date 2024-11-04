e7103d5dbb8eafcf72b8855cc2af982fb62e1a93
Manuel Gomez <jmgc@virket.com>
2024-11-04 09:18:20 -0600

    Redireccionar al Dashboard si estamos autenticados

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,16 +1,32 @@
 
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthStore } from '../stores';
 
 export const AuthLayout = () => {
+
+  const authStatus = useAuthStore((state) => state.status);
+  const checkAuthStatus = useAuthStore((state) => state.checkAuthStatus);
+  console.log(authStatus);
+
+
+  if (authStatus === 'pending') {
+    checkAuthStatus();
+    return <div>Loading...</div>;
+  }
+
+  if (authStatus === 'authorized') {
+    return <Navigate to="/dashboard" />;
+  }
+
   return (
-    <div className="bg-gray-100 flex justify-center items-center h-screen">
-      <div className="w-1/2 h-screen hidden lg:flex lg:flex-col items-center justify-center bg-indigo-700">
-        <span className="text-white font-bold text-9xl">Zustand</span>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="items-center justify-center hidden w-1/2 h-screen bg-indigo-700 lg:flex lg:flex-col">
+        <span className="font-bold text-white text-9xl">Zustand</span>
         {/* <img src="https://placehold.co/1440/667fff/ffffff.png?text=Zustand&font=Montserrat"
           alt="Placeholder Image"
           className="object-cover w-full h-full" /> */}
       </div>
-      <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
+      <div className="w-full p-8 lg:p-36 md:p-52 sm:20 lg:w-1/2">
         <Outlet />
       </div>
     </div>
